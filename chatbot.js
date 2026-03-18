770d10fa51229f71a633b7c4c058a7161adc2e8b
Nijil Chandran <nijil.mail@gmail.com>
2026-03-18 10:06:09 +0530

    chatbot

---
+++ chatbot.js
@@ -0,0 +1,11 @@
+<script async
+  src="https://dou3ota6tj4frnt5zfltu2fv.agents.do-ai.run/static/chatbot/widget.js"
+  data-agent-id="71c8fc0d-227e-11f1-b074-4e013e2ddde4"
+  data-chatbot-id="iTabyc1nI15PjUFWZXPu8Bo1tEsRaCdu"
+  data-name="inba-deductionhelper Chatbot"
+  data-primary-color="#031B4E"
+  data-secondary-color="#E5E8ED"
+  data-button-background-color="#0061EB"
+  data-starting-message="Hello!  I'm Inba. How can I help you with your insurance claim today?"
+  data-logo="/static/chatbot/icons/default-agent.svg">
+</script>
